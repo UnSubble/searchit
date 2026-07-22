57cb9c19efd07b801ba5430c4dbd43bee941c8e0
unsubble <unsubble@gmail.com>
2026-07-22 05:27:21 +0300

    [REFINE] Update version to v0.5.0: Changed the version variable in build script and version.go for consistency.

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ const Name = "searchit"
 
 // Defined as variables so they can be overridden via -ldflags at build time.
 var (
-	Version = "v0.4.0"
+	Version = "v0.5.0"
 	Commit  = "dev"
 	Date    = "unknown"
 )
