614afe4f766970c7702bef881ec5b3d36a858147
unsubble <unsubble@gmail.com>
2026-07-25 15:04:19 +0300

    [RELEASE] Bump version to v0.5.4; enhance update experience and clarify contextual messages

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ const Name = "searchit"
 
 // Defined as variables so they can be overridden via -ldflags at build time.
 var (
-	Version = "v0.5.3"
+	Version = "v0.5.4"
 	Commit  = "dev"
 	Date    = "unknown"
 )
