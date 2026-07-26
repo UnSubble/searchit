be3ef834eed90edd830b083751a01e4f798a6ac3
unsubble <unsubble@gmail.com>
2026-07-26 12:13:42 +0300

    [RELEASE] Prepare v0.5.5 release

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ const Name = "searchit"
 
 // Defined as variables so they can be overridden via -ldflags at build time.
 var (
-	Version = "v0.5.5-dev"
+	Version = "v0.5.5"
 	Commit  = "dev"
 	Date    = "unknown"
 )
