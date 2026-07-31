d6ba11d92a00301194243206ba9eff5e0d6527eb
unsubble <unsubble@gmail.com>
2026-07-31 23:27:03 +0300

    [UPDATE] Bump version to v0.5.9-dev in version.go

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ const Name = "searchit"
 
 // Defined as variables so they can be overridden via -ldflags at build time.
 var (
-	Version = "v0.5.8"
+	Version = "v0.5.9-dev"
 	Commit  = "dev"
 	Date    = "unknown"
 )
